7e2cb544b3c7f68f14f91c63658ee9a0bffb89d2
Durgarao418 <durgaraoravuri418@gmail.com>
2025-07-06 02:39:00 +0530

    Initial Commit

---
+++ src/main/java/com/smartboats/customerservice/controller/Customer.java
@@ -0,0 +1,15 @@
+package com.smartboats.customerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Customer
+{
+    @GetMapping("/getCustomers")
+    public String getCustomers()
+    {
+        return "List of customers";
+    }
+
+}
---
+++ src/test/java/com/smartboats/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartboats.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
